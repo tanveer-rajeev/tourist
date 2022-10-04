c263147a628778df0c822e03f249b26481f5c0de
rajeeb <rajeeb@bitmascot.com>
2022-10-04 12:27:36 +0600

    mongo cloud

--- server/index.js
+++ server/index.js
@@ -16,20 +16,22 @@ app.use("/posts", postRoutes);
 app.use("/users",userRouter);
 
 
-mongoose
-  .connect("mongodb://localhost/memories")
-  .then((res) => console.log("Connection successfully"))
-  .catch((err) => console.log(err));
-// const CONNECTION_URL =
-//   "mongodb+srv://rajeeb:rajeeb123@cluster0.imzgboa.mongodb.net/?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 5000;
 // mongoose
-//   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() =>
-//     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
-//   )
-//   .catch((err) => console.log(err.message));
-// mongoose.set("useFindAndModify", false);
+//   .connect("mongodb://localhost/memories")
+//   .then((res) => console.log("Connection successfully"))
+//   .catch((err) => console.log(err));
+
+const CONNECTION_URL = process.env.MONGOURI;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((err) => console.log(err.message));
+mongoose.set("useFindAndModify", false);
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
